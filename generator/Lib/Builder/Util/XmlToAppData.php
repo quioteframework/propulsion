c8005a21346003b0702c8dbb8cb6f094bdcd658d
Markus Lervik <markus.lervik@thejakamo.com>
2026-07-30 18:00:42 +0300

    Builder/Util: fix level 9 findings (XmlToAppData.php ~44 -> 0, StandardEnglishPluralizer.php 3 -> 0)
    Add local requireCurrent*() guards for the XML-parser's
    child-tag-dispatch invariants, and fix a real bug where the
    <foreign-key><vendor> branch attached vendor info to currUnique
    instead of currFK.

--- generator/Lib/Builder/Util/XmlToAppData.php
+++ generator/Lib/Builder/Util/XmlToAppData.php
@@ -74,7 +74,7 @@ class XmlToAppData
 	 * @param      string $defaultPackage the default PHP package used for the om
 	 * @param      string $encoding The database encoding.
 	 */
-	public function __construct(?PropulsionPlatformInterface $defaultPlatform = null, $defaultPackage = null, $encoding = 'iso-8859-1')
+	public function __construct(?PropulsionPlatformInterface $defaultPlatform = null, ?string $defaultPackage = null, string $encoding = 'iso-8859-1')
 	{
 		$this->app = new AppData($defaultPlatform);
 		$this->defaultPackage = $defaultPackage;
@@ -98,14 +98,19 @@ class XmlToAppData
 	 * @param      string $xmlFile The input file to parse.
 	 * @return     AppData populated by <code>xmlFile</code>.
 	 */
-	public function parseFile($xmlFile)
+	public function parseFile(string $xmlFile): AppData
 	{
 		// we don't want infinite recursion
 		if ($this->isAlreadyParsed($xmlFile)) {
 			return $this->app;
 		}
 
-		return $this->parseString(file_get_contents($xmlFile), $xmlFile);
+		$xmlString = file_get_contents($xmlFile);
+		if ($xmlString === false) {
+			throw new SchemaException(sprintf('Unable to read schema file "%s"', $xmlFile));
+		}
+
+		return $this->parseString($xmlString, $xmlFile);
 	}
 
 	/**
@@ -116,7 +121,7 @@ class XmlToAppData
 	 * @param      string $xmlFile The input file name.
 	 * @return     AppData populated by <code>xmlFile</code>.
 	 */
-	public function parseString($xmlString, $xmlFile = null)
+	public function parseString(string $xmlString, ?string $xmlFile = null): AppData
 	{
 		$xmlFile ??= '';
 		// we don't want infinite recursion
@@ -144,7 +149,7 @@ class XmlToAppData
 	/**
 	 * Handles opening elements of the xml file.
 	 *
-	 * @param      resource|\XMLParser $parser The XML parser resource/instance.
+	 * @param      \XMLParser $parser The XML parser instance.
 	 * @param      string $name The name of the element.
 	 * @param      array<string, mixed> $attributes The specified or defaulted attributes.
 	 */
@@ -157,10 +162,8 @@ class XmlToAppData
 				switch($name) {
 					case "database":
 						if ($this->isExternalSchema()) {
-							$this->currentPackage = @$attributes["package"];
-							if ($this->currentPackage === null) {
-								$this->currentPackage = $this->defaultPackage;
-							}
+							$package = $attributes["package"] ?? null;
+							$this->currentPackage = is_string($package) ? $package : $this->defaultPackage;
 						} else {
 							$this->currDB = $this->app->addDatabase($attributes);
 						}
@@ -175,43 +178,48 @@ class XmlToAppData
 			switch($name) {
 
 				case "external-schema":
-					$xmlFile = @$attributes["filename"];
+					$filename = $attributes["filename"] ?? null;
+					if (!is_string($filename)) {
+						throw new SchemaException('Missing required "filename" attribute on <external-schema> element');
+					}
+					$xmlFile = $filename;
 
 					// "referenceOnly" attribute is valid in the main schema XML file only,
 					// and it's ignored in the nested external-schemas
 					if (!$this->isExternalSchema()) {
-						$isForRefOnly = @$attributes["referenceOnly"];
-						$this->isForReferenceOnly = ($isForRefOnly !== null ? (strtolower($isForRefOnly) === "true") : true); // defaults to TRUE
+						$isForRefOnly = $attributes["referenceOnly"] ?? null;
+						$this->isForReferenceOnly = (is_string($isForRefOnly) ? (strtolower($isForRefOnly) === "true") : true); // defaults to TRUE
 					}
 
 					if ($xmlFile[0] != '/') {
-						$xmlFile = realpath(dirname($this->currentXmlFile) . DIRECTORY_SEPARATOR . $xmlFile);
-						if (!file_exists($xmlFile)) {
+						$resolved = realpath(dirname($this->requireCurrentXmlFile()) . DIRECTORY_SEPARATOR . $xmlFile);
+						if ($resolved === false || !file_exists($resolved)) {
 							throw new SchemaException(sprintf('Unknown include external "%s"', $xmlFile));
 						}
+						$xmlFile = $resolved;
 					}
 
 					$this->parseFile($xmlFile);
 				break;
 
   		  case "domain":
-				  $this->currDB->addDomain($attributes);
+				  $this->requireCurrentDatabase()->addDomain($attributes);
 			  break;
 
 				case "table":
-					$this->currTable = $this->currDB->addTable($attributes);
+					$this->currTable = $this->requireCurrentDatabase()->addTable($attributes);
 					if ($this->isExternalSchema()) {
-						$this->currTable->setForReferenceOnly($this->isForReferenceOnly);
+						$this->currTable->setForReferenceOnly($this->isForReferenceOnly ?? true);
 						$this->currTable->setPackage($this->currentPackage);
 					}
 				break;
 
 				case "vendor":
-					$this->currVendorObject = $this->currDB->addVendorInfo($attributes);
+					$this->currVendorObject = $this->requireCurrentDatabase()->addVendorInfo($attributes);
 				break;
 
 				case "behavior":
-				  $this->currBehavior = $this->currDB->addBehavior($attributes);
+				  $this->currBehavior = $this->requireCurrentDatabase()->addBehavior($attributes);
 				break;
 
 				default:
@@ -222,39 +230,39 @@ class XmlToAppData
 
 			switch($name) {
 				case "column":
-					$this->currColumn = $this->currTable->addColumn($attributes);
+					$this->currColumn = $this->requireCurrentTable()->addColumn($attributes);
 				break;
 
 				case "foreign-key":
-					$this->currFK = $this->currTable->addForeignKey($attributes);
+					$this->currFK = $this->requireCurrentTable()->addForeignKey($attributes);
 				break;
 
 				case "index":
-					$this->currIndex = $this->currTable->addIndex($attributes);
+					$this->currIndex = $this->requireCurrentTable()->addIndex($attributes);
 				break;
 
 				case "unique":
-					$this->currUnique = $this->currTable->addUnique($attributes);
+					$this->currUnique = $this->requireCurrentTable()->addUnique($attributes);
 				break;
 
 				case "exclusion":
-					$this->currExclusion = $this->currTable->addExclusion($attributes);
+					$this->currExclusion = $this->requireCurrentTable()->addExclusion($attributes);
 				break;
 
 				case "vendor":
-					$this->currVendorObject = $this->currTable->addVendorInfo($attributes);
+					$this->currVendorObject = $this->requireCurrentTable()->addVendorInfo($attributes);
 				break;
 
 	  		case "validator":
-				  $this->currValidator = $this->currTable->addValidator($attributes);
+				  $this->currValidator = $this->requireCurrentTable()->addValidator($attributes);
 	  		break;
 
 	  		case "id-method-parameter":
-					$this->currTable->addIdMethodParameter($attributes);
+					$this->requireCurrentTable()->addIdMethodParameter($attributes);
 				break;
 
 				case "behavior":
-				  $this->currBehavior = $this->currTable->addBehavior($attributes);
+				  $this->currBehavior = $this->requireCurrentTable()->addBehavior($attributes);
 				break;
 
 				default:
@@ -265,11 +273,11 @@ class XmlToAppData
 
 			switch($name) {
 				case "inheritance":
-					$this->currColumn->addInheritance($attributes);
+					$this->requireCurrentColumn()->addInheritance($attributes);
 				break;
 
 				case "vendor":
-					$this->currVendorObject = $this->currColumn->addVendorInfo($attributes);
+					$this->currVendorObject = $this->requireCurrentColumn()->addVendorInfo($attributes);
 				break;
 
 				default:
@@ -280,11 +288,11 @@ class XmlToAppData
 
 			switch($name) {
 				case "reference":
-					$this->currFK->addReference($attributes);
+					$this->requireCurrentForeignKey()->addReference($attributes);
 				break;
 
 				case "vendor":
-					$this->currVendorObject = $this->currUnique->addVendorInfo($attributes);
+					$this->currVendorObject = $this->requireCurrentForeignKey()->addVendorInfo($attributes);
 				break;
 
 				default:
@@ -295,11 +303,11 @@ class XmlToAppData
 
 			switch($name) {
 				case "index-column":
-					$this->currIndex->addColumn($attributes);
+					$this->requireCurrentIndex()->addColumn($attributes);
 				break;
 
 				case "vendor":
-					$this->currVendorObject = $this->currIndex->addVendorInfo($attributes);
+					$this->currVendorObject = $this->requireCurrentIndex()->addVendorInfo($attributes);
 				break;
 
 				default:
@@ -310,11 +318,11 @@ class XmlToAppData
 
 			switch($name) {
 				case "unique-column":
-					$this->currUnique->addColumn($attributes);
+					$this->requireCurrentUnique()->addColumn($attributes);
 				break;
 
 				case "vendor":
-					$this->currVendorObject = $this->currUnique->addVendorInfo($attributes);
+					$this->currVendorObject = $this->requireCurrentUnique()->addVendorInfo($attributes);
 				break;
 
 				default:
@@ -324,11 +332,11 @@ class XmlToAppData
 
 			switch($name) {
 				case "exclusion-column":
-					$this->currExclusion->addColumn($attributes);
+					$this->requireCurrentExclusion()->addColumn($attributes);
 				break;
 
 				case "vendor":
-					$this->currVendorObject = $this->currExclusion->addVendorInfo($attributes);
+					$this->currVendorObject = $this->requireCurrentExclusion()->addVendorInfo($attributes);
 				break;
 
 				default:
@@ -338,7 +346,7 @@ class XmlToAppData
 
 			switch($name) {
 				case "parameter":
-					$this->currBehavior->addParameter($attributes);
+					$this->requireCurrentBehavior()->addParameter($attributes);
 				break;
 
 				default:
@@ -347,7 +355,7 @@ class XmlToAppData
 		} elseif ($parentTag == "validator") {
 			switch($name) {
 				case "rule":
-					$this->currValidator->addRule($attributes);
+					$this->requireCurrentValidator()->addRule($attributes);
 				break;
 				default:
 					$this->_throwInvalidTagException($parser, $name);
@@ -356,7 +364,7 @@ class XmlToAppData
 
 			switch($name) {
 				case "parameter":
-					$this->currVendorObject->addParameter($attributes);
+					$this->requireCurrentVendorObject()->addParameter($attributes);
 				break;
 				default:
 					$this->_throwInvalidTagException($parser, $name);
@@ -371,7 +379,7 @@ class XmlToAppData
 	}
 
 	/**
-	 * @param      resource|\XMLParser $parser The XML parser resource/instance.
+	 * @param      \XMLParser $parser The XML parser instance.
 	 * @param      string $tag_name The name of the unexpected tag.
 	 */
 	function _throwInvalidTagException($parser, string $tag_name): never
@@ -390,7 +398,7 @@ class XmlToAppData
 	/**
 	 * Handles closing elements of the xml file.
 	 *
-	 * @param      resource|\XMLParser $parser The XML parser resource/instance.
+	 * @param      \XMLParser $parser The XML parser instance.
 	 * @param      string $name The name of the element.
 	 */
 	public function endElement($parser, $name): void
@@ -400,20 +408,31 @@ class XmlToAppData
 
 	protected function peekCurrentSchemaTag(): string|false
 	{
-		$keys = array_keys($this->schemasTagsStack);
-		return end($this->schemasTagsStack[end($keys)]);
+		return end($this->schemasTagsStack[$this->currentSchemaKey()]);
 	}
 
 	protected function popCurrentSchemaTag(): void
 	{
-		$keys = array_keys($this->schemasTagsStack);
-		array_pop($this->schemasTagsStack[end($keys)]);
+		array_pop($this->schemasTagsStack[$this->currentSchemaKey()]);
 	}
 
 	protected function pushCurrentSchemaTag(string $tag): void
 	{
+		$this->schemasTagsStack[$this->currentSchemaKey()][] = $tag;
+	}
+
+	/**
+	 * Returns the key (schema file path) of the schema currently being
+	 * parsed, i.e. the top of the nested-parsing stack.
+	 */
+	private function currentSchemaKey(): string
+	{
 		$keys = array_keys($this->schemasTagsStack);
-		$this->schemasTagsStack[end($keys)][] = $tag;
+		$key = end($keys);
+		if ($key === false) {
+			throw new SchemaException('No schema file is currently being parsed');
+		}
+		return $key;
 	}
 
 	protected function isExternalSchema(): bool
@@ -424,5 +443,93 @@ class XmlToAppData
 	protected function isAlreadyParsed(string $filePath): bool
 	{
 		return isset($this->schemasTagsStack[$filePath]);
+	}
+
+	private function requireCurrentXmlFile(): string
+	{
+		if ($this->currentXmlFile === null) {
+			throw new SchemaException('No current schema file is being parsed');
+		}
+		return $this->currentXmlFile;
+	}
+
+	private function requireCurrentDatabase(): Database
+	{
+		if ($this->currDB === null) {
+			throw new SchemaException('Encountered a <database> child element without an enclosing <database> element');
+		}
+		return $this->currDB;
+	}
+
+	private function requireCurrentTable(): Table
+	{
+		if ($this->currTable === null) {
+			throw new SchemaException('Encountered a <table> child element without an enclosing <table> element');
+		}
+		return $this->currTable;
+	}
+
+	private function requireCurrentColumn(): Column
+	{
+		if ($this->currColumn === null) {
+			throw new SchemaException('Encountered a <column> child element without an enclosing <column> element');
+		}
+		return $this->currColumn;
+	}
+
+	private function requireCurrentForeignKey(): ForeignKey
+	{
+		if ($this->currFK === null) {
+			throw new SchemaException('Encountered a <foreign-key> child element without an enclosing <foreign-key> element');
+		}
+		return $this->currFK;
+	}
+
+	private function requireCurrentIndex(): Index
+	{
+		if ($this->currIndex === null) {
+			throw new SchemaException('Encountered an <index> child element without an enclosing <index> element');
+		}
+		return $this->currIndex;
+	}
+
+	private function requireCurrentUnique(): Unique
+	{
+		if ($this->currUnique === null) {
+			throw new SchemaException('Encountered a <unique> child element without an enclosing <unique> element');
+		}
+		return $this->currUnique;
+	}
+
+	private function requireCurrentExclusion(): Exclusion
+	{
+		if ($this->currExclusion === null) {
+			throw new SchemaException('Encountered an <exclusion> child element without an enclosing <exclusion> element');
+		}
+		return $this->currExclusion;
+	}
+
+	private function requireCurrentValidator(): Validator
+	{
+		if ($this->currValidator === null) {
+			throw new SchemaException('Encountered a <validator> child element without an enclosing <validator> element');
+		}
+		return $this->currValidator;
+	}
+
+	private function requireCurrentBehavior(): Behavior
+	{
+		if ($this->currBehavior === null) {
+			throw new SchemaException('Encountered a <behavior> child element without an enclosing <behavior> element');
+		}
+		return $this->currBehavior;
+	}
+
+	private function requireCurrentVendorObject(): VendorInfo
+	{
+		if ($this->currVendorObject === null) {
+			throw new SchemaException('Encountered a <vendor> child element without an enclosing <vendor> element');
+		}
+		return $this->currVendorObject;
 	}
 }
